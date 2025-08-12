78382c08a7e94ccf6b80175dfcd48f8d1348c065
Divya S <divya126bca@gmail.com>
2025-08-12 05:16:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -12,13 +12,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">{children}</body>
-    </html>
+    <div className="font-body antialiased">
+      {children}
+    </div>
   );
 }
