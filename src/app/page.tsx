7664ccca8ef98217735369c6422ba5ddeaade920
Divya S <divya126bca@gmail.com>
2025-11-20 11:49:36 +0000

    change this one to academic-record.gif  (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -568,7 +568,7 @@ export default function Home() {
   const whyChooseUsPoints = [
     {
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-armchair text-primary"><path d="M19 9V6a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v3"/><path d="M3 16a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2Z"/><path d="M5 18v2"/><path d="M19 18v2"/></svg>
+        <Image src="/academic-record.gif" alt="Academic Record" width={60} height={60} />
       ),
       title: "Daily interaction with parents through Academic Record.",
       description: "Justo non dolor lectus ac egestas dictum. Leo tempus nec amet fringilla.",
@@ -1172,6 +1172,7 @@ export default function Home() {
     
 
     
+
 
 
 
