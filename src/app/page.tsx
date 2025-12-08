b5998c4bdd5877d3d7084c68f5e003fbdee417b2
Divya S <divya126bca@gmail.com>
2025-12-08 06:54:36 +0000

    change this last phone button to whatsapp  (_for element <Button>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -706,7 +706,7 @@ export default function Home() {
               <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
                 {exploreCourses.map((course, index) => (
                   <div key={index} className="group [perspective:1000px]">
-                    <div className="relative w-28 h-28 mx-auto -mb-14 z-10">
+                     <div className="relative w-28 h-28 mx-auto -mb-14 z-10">
                         <div className="relative w-full h-full [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] transition-transform duration-1000">
                             {/* Front Face */}
                             <div 
@@ -1191,7 +1191,20 @@ export default function Home() {
             <Phone className="w-6 h-6 text-white" />
           </Button>
           <Button size="icon" className="rounded-full bg-green-500 hover:bg-green-600 w-12 h-12">
-            <Phone className="w-6 h-6 text-white" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="w-6 h-6 text-white"
+            >
+              <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
+            </svg>
           </Button>
       </div>
     </div>
@@ -1203,3 +1216,4 @@ export default function Home() {
 
 
     
+
