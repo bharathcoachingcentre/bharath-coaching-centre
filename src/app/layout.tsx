eb0a2e0ef3ea0dd597ef3fd732c1dff13c87ecde
Divyabharathi.S <divyashan011@gmail.com>
2026-04-10 05:24:06 +0000

    My favicon request is returning HTML instead of the .ico file (Content-T

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
@@ -18,11 +19,9 @@ export const metadata: Metadata = {
   title: 'Bharath Academy Hub',
   description: 'Welcome to Bharath Academy',
   icons: {
-    icon: [
-      { url: '/fav.ico?v=3', type: 'image/x-icon' },
-    ],
-    shortcut: '/fav.ico?v=3',
-    apple: '/fav.ico?v=3',
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
   },
 };
 
@@ -33,14 +32,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="light">
-      <head>
-        {/* Aggressive Firefox Cache-Bypass & MIME satisfaction */}
-        <link rel="icon" href="/fav.ico?v=3" type="image/x-icon" sizes="any" />
-        <link rel="shortcut icon" href="/fav.ico?v=3" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="/fav.ico?v=3" />
-        {/* Support for Firefox's alternate MIME recognition */}
-        <link rel="icon" href="/fav.ico?v=3" type="image/vnd.microsoft.icon" />
-      </head>
       <body className={`${inter.variable} font-body antialiased flex flex-col min-h-screen`}>
         <FirebaseClientProvider>
           <ClientHeader />
